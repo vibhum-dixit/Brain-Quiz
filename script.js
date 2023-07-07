f91bd7dfedc6116f28d7e54f38618215078872e2
Vibhum Ranjan Dixit <124594421+vibhum-dixit@users.noreply.github.com>
2023-07-07 14:40:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,100 @@
+const quizData = [
+    {
+        question: "Who developed c programming language ?",
+        a: "Dennis Ritchie ",
+        b: "Ken Thompson",
+        c: "Brian Kernighan",
+        d: "Bjarne Stroustrup",
+        correct: "a",
+    },
+    {
+        question: "What is full form of SASS?",
+        a: "Syntactically Awesome Style Sheets",
+        b: "Superior Awesome Style Sheet",
+        c: "Statiscally Awesome Style Sheet",
+        d: "Syntaxically Awesome Style Sheets",
+        correct: "a",
+    },
+    {
+        question: "In which year java was developed?",
+        a: "1996",
+        b: "1995",
+        c: "1994",
+        d: "none of the above",
+        correct: "b",
+    },
+    {
+        question: "Who developed first Computer ?",
+        a:"Charles Babbage",
+        b:"Smith thompson",
+        c:"john wick",
+        d:"stuard troat",
+        correct:"a",
+    },
+];
+
+const quiz = document.getElementById('quiz')
+const answerEls = document.querySelectorAll('.answer')
+const questionEl = document.getElementById('question')
+const a_text = document.getElementById('a_text')
+const b_text = document.getElementById('b_text')
+const c_text = document.getElementById('c_text')
+const d_text = document.getElementById('d_text')
+
+const submitBtn = document.getElementById('submit')
+
+let currentQuiz = 0
+let score = 0
+
+loadQuiz()
+
+function loadQuiz() {
+    deselectAnswers()
+
+    const currentQuizData = quizData[currentQuiz]
+
+    questionEl.innerText = currentQuizData.question
+    a_text.innerText = currentQuizData.a
+    b_text.innerText = currentQuizData.b
+    c_text.innerText = currentQuizData.c
+    d_text.innerText = currentQuizData.d
+
+}
+
+function deselectAnswers() {
+    answerEls.forEach(answerEl => answerEl.checked = false)
+}
+
+function getSelected() {
+    let answer
+
+    answerEls.forEach(answerEl => {
+        if(answerEl.checked) {
+            answer = answerEl.id
+        }
+    })
+
+    return answer
+}
+
+submitBtn.addEventListener('click', () => {
+    const answer = getSelected()
+    
+    if(answer) {
+        if(answer === quizData[currentQuiz].correct) {
+            score++
+        }
+
+        currentQuiz++
+
+        if(currentQuiz < quizData.length) {
+            loadQuiz()
+        } else {
+            quiz.innerHTML = `
+                <h2>You answered ${score}/${quizData.length} questions correctly</h2>
+
+                <button onclick="location.reload()">Reload</button>
+            `
+        }
+    }
+})
